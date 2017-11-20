2f1f945082b729db7b0807f06b49824b1dc56f88
Emily Jones <efhjones@gmail.com>
2017-11-19 17:52:52 -0700

    add graphql query to container

--- pool-app/app/AppContainer.js
+++ pool-app/app/AppContainer.js
@@ -1,19 +1,51 @@
+import { graphql } from 'react-apollo';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+import gql from 'graphql-tag';
 import App from './App';
 import AppActions from './actions';
 
-const mapStateToProps = state => ({
-  text: state.app.text
-});
+const mapStateToProps = (state) => {
+  const { app } = state;
+  const { isErrored, errorMessage } = app;
+  return {
+    isErrored,
+    errorMessage,
+    text: state.app.text
+  };
+};
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch, props) => ({
+  onSignUp(email, password) {
+    props.signUpUser({ variables: { email, password } })
+      .then(({ data }) => {
+        const { signupUser } = data;
+        const { id, token } = signupUser;
+        dispatch(AppActions.onSetId(id));
+        dispatch(AppActions.onSaveToken(token));
+      }).catch((error) => {
+        dispatch(AppActions.onSetErrors(error));
+      });
+  },
   onChangeText(text) {
     dispatch(AppActions.onChangeText(text));
   },
-  onSignUp(username, password) {
-    dispatch(AppActions.onSignUp(username, password));
+  onClearErrors() {
+    dispatch(AppActions.onClearErrors());
   }
-
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+const SIGNUP_EMAIL_USER = gql`
+  mutation SignupUser($email: String!, $password: String!) {
+    signupUser(email: $email, password: $password) {
+      id
+      token
+    }
+  }
+`;
+
+
+export default _.flowRight(
+  graphql(SIGNUP_EMAIL_USER, { name: 'signUpUser' }),
+  connect(mapStateToProps, mapDispatchToProps)
+)(App);
