368a0256ca4fd69419263568534db7d35ae935ae
Emily Jones <efhjones@gmail.com>
2017-11-19 17:49:41 -0700

    Add actions for signup flow

--- pool-app/app/actions.js
+++ pool-app/app/actions.js
@@ -1,11 +1,34 @@
+import AsyncStorage from 'AsyncStorage';
 import * as ActionTypes from './actionTypes';
 
 const AppActions = {
-  onChangeText(text) {
+  onSetId(id) {
     return {
-      type: ActionTypes.CHANGE_TEXT,
-      text
-    }
+      type: ActionTypes.SET_AUTH_ID,
+      id
+    };
+  },
+  onSaveToken(token) {
+    return dispatch => AsyncStorage.setItem('UserAuthToken', token, (err) => {
+      if (err) {
+        return dispatch(AppActions.onSetErrors(err));
+      }
+      dispatch({
+        type: ActionTypes.ON_SAVE_TOKEN,
+        token
+      });
+    });
+  },
+  onSetErrors(errors) {
+    return {
+      type: ActionTypes.ON_SET_ERRORS,
+      errors
+    };
+  },
+  onClearErrors() {
+    return {
+      type: ActionTypes.CLEAR_ERRORS
+    };
   }
 };
 
