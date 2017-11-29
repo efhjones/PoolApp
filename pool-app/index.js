ff7cfa5352c49be80097f2005b2f48d5f4de1992
Emily Jones <efhjones@gmail.com>
2017-11-28 23:05:32 -0700

    Add app authorization query

--- pool-app/index.js
+++ pool-app/index.js
@@ -5,16 +5,41 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { ApolloClient } from 'apollo-client';
+import AsyncStorage from 'AsyncStorage';
 
 import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import reducer from './reducer/index';
 import NavigationContainer from './navigation/NavigationContainer';
 
+const httpLink = createHttpLink({
+  uri: 'https://api.graph.cool/simple/v1/cja6b9y0y07q50129zgdjekpk'
+});
+
+const getAuthToken = async () => {
+  try {
+    const token = await AsyncStorage.getItem('UserAuthToken');
+    return token;
+  } catch (error) {
+    return error;
+  }
+};
+
+const middlewareLink = setContext(async () => {
+  const authToken = await getAuthToken();
+  return ({
+    headers: {
+      authorization: `Bearer ${authToken}` || null
+    }
+  });
+});
+
+const link = middlewareLink.concat(httpLink);
 
 const client = new ApolloClient({
-  link: createHttpLink({ uri: 'https://api.graph.cool/simple/v1/cja6b9y0y07q50129zgdjekpk' }),
+  link,
   cache: new InMemoryCache()
 });
 
