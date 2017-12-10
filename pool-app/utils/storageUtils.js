5f47558a86e742326b6b6dfae3825473efcb3f98
Emily Jones <efhjones@gmail.com>
2017-12-09 20:19:36 -0700

    add storage util for game token

--- pool-app/utils/storageUtils.js
+++ pool-app/utils/storageUtils.js
@@ -1,9 +1,17 @@
 import AsyncStorage from 'AsyncStorage';
-import { STORAGE_AUTH_TOKEN } from './constants';
+import { STORAGE_AUTH_TOKEN, CURRENT_GAME_TOKEN } from './constants';
 
 export const getToken = async () => {
   try {
     return await AsyncStorage.getItem(STORAGE_AUTH_TOKEN);
+  } catch (err) {
+    return err;
+  }
+};
+
+export const saveGameToken = async (gameId) => {
+  try {
+    return await AsyncStorage.setItem(CURRENT_GAME_TOKEN, gameId);
   } catch (err) {
     return err;
   }
