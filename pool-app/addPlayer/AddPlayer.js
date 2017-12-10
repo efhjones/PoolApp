5682668eece2f85d4a86cad60d9751692e8026c9
Emily Jones <efhjones@gmail.com>
2017-12-10 01:03:48 -0700

    connect graphql to AddPlayer

--- pool-app/addPlayer/AddPlayer.js
+++ pool-app/addPlayer/AddPlayer.js
@@ -1,21 +1,50 @@
 import React from 'react';
-
-import { StyleSheet, View, Button, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { StyleSheet, View, Button, Text, TextInput } from 'react-native';
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
+  },
+  field: {
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: 200,
+    marginBottom: 10,
+    padding: 10
   }
 });
 
-const AddPlayer = () => (
+const AddPlayer = ({
+  onAddPlayer, onUpdateName, name, gameId
+}) => (
   <View style={styles.container}>
-    <Text>I&rsquo;m the add player screen!</Text>
+    <TextInput
+      onChangeText={onUpdateName}
+      value={name}
+      style={styles.field}
+      autoCapitalize="none"
+      placeholder="Player Name"
+    />
+    <Button
+      style={styles.createAccountButton}
+      onPress={() => onAddPlayer({ gameId, name })}
+      title="Add Player to Game"
+    />
   </View>
 );
+
+AddPlayer.propTypes = {
+  onAddPlayer: PropTypes.func.isRequired,
+  onUpdateName: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  gameId: PropTypes.string.isRequired
+};
+
 
 export default AddPlayer;
--- pool-app/addPlayer/AddPlayerContainer.js
+++ pool-app/addPlayer/AddPlayerContainer.js
@@ -1,8 +1,49 @@
 import { connect } from 'react-redux';
+import _ from 'lodash';
+import { withStateHandlers } from 'recompose';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import AddPlayer from './AddPlayer';
 
-const mapStateToProps = () => ({});
 
-const mapDispatchToProps = () => ({});
+const mapStateToProps = (state) => {
+  const { home } = state;
+  const { gameId } = home;
+  return {
+    gameId
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddPlayer);
+const mapDispatchToProps = (dispatch, props) => ({
+  onAddPlayer({ gameId, name }) {
+    props.addPlayerToGame({ variables: { gameId, name } })
+      .then((response) => {
+        debugger;
+      }).catch((err) => {
+        debugger;
+      });
+  }
+});
+
+const ADD_PLAYER_TO_GAME = gql`
+mutation AddPlayerToGame($gameId: String!, $name: String!, $email: String) {
+  addToUserInGame(gamesGameId: $gameId, name: $name, email: $email) {
+    id
+  }
+}
+`;
+
+const initialState = {
+  name: ''
+};
+
+const handlers = {
+  onUpdateName: () => name => ({ name })
+};
+
+
+export default _.flowRight(
+  graphql(ADD_PLAYER_TO_GAME, { name: 'addPlayerToGame' }),
+  connect(mapStateToProps, mapDispatchToProps),
+  withStateHandlers(initialState, handlers),
+)(AddPlayer);
