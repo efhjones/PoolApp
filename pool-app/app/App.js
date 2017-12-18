9511b9f00ed73cc84ca74eacc12f4bad9b835636
Emily Jones <efhjones@gmail.com>
2017-12-18 01:53:26 -0700

    make login slightly more tolerable to look at

--- pool-app/app/App.js
+++ pool-app/app/App.js
@@ -12,17 +12,18 @@ import PropTypes from 'prop-types';
 import Error from '../error/Error';
 import HomeContainer from '../home/HomeContainer';
 import Loading from '../loading/Loading';
+import { MIDNIGHT_BLUE, PLATINUM, PARADISE_PINK } from '../styles/constants';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: MIDNIGHT_BLUE
   },
   field: {
     height: 40,
-    borderColor: 'grey',
+    borderColor: PLATINUM,
     borderWidth: 1,
     width: 200,
     marginBottom: 10,
@@ -32,7 +33,34 @@ const styles = StyleSheet.create({
     display: 'flex'
   },
   createAccountButton: {
-    display: 'flex'
+    display: 'flex',
+    backgroundColor: PLATINUM
+  },
+  createAccountButtonContainer: {
+    display: 'flex',
+    backgroundColor: PARADISE_PINK
+  },
+  signInButtonContainer: {
+    backgroundColor: PLATINUM,
+    display: 'flex',
+    borderRadius: 300
+  },
+  buttonRow: {
+    flexDirection: 'column',
+    display: 'flex',
+    width: 200
+  },
+  or: {
+    color: PLATINUM,
+    textAlign: 'center',
+    fontWeight: '600',
+    padding: 10
+  },
+  header: {
+    fontSize: 50,
+    color: PLATINUM,
+    paddingTop: 20,
+    paddingBottom: 20
   }
 });
 
@@ -48,38 +76,50 @@ const LoggedOutView = ({
   onLogIn
 }) => (
   <View style={styles.container}>
-    <Text>Sign in or create a new account below</Text>
+    <Text style={styles.header}>Poolie</Text>
     <TextInput
       onChangeText={onChangeEmail}
       onFocus={onClearErrors}
       value={email}
       style={styles.field}
+      placeholderTextColor={PLATINUM}
       autoCapitalize="none"
       placeholder="email"
       keyboardType="email-address"
     />
     <TextInput
       onChangeText={onChangePassword}
+      placeholderTextColor={PLATINUM}
       onFocus={onClearErrors}
       value={password}
       style={styles.field}
       placeholder="password"
+      color={PLATINUM}
     />
     {
       isErrored
       ? <Error error={errorMessage} />
       : null
     }
-    <Button
-      style={styles.createAccountButton}
-      onPress={() => onSignUp(email, password)}
-      title="Create an Account"
-    />
-    <Button
-      style={styles.signInButton}
-      title="Sign In"
-      onPress={() => onLogIn(email, password)}
-    />
+    <View style={styles.buttonRow}>
+      <View style={styles.signInButtonContainer}>
+        <Button
+          style={styles.signInButton}
+          title="Sign In"
+          color={MIDNIGHT_BLUE}
+          onPress={() => onLogIn(email, password)}
+        />
+      </View>
+      <Text style={styles.or}>- OR -</Text>
+      <View style={styles.createAccountButtonContainer}>
+        <Button
+          style={styles.createAccountButton}
+          onPress={() => onSignUp(email, password)}
+          title="Create an Account"
+          color={PLATINUM}
+        />
+      </View>
+    </View>
   </View>
 );
 
