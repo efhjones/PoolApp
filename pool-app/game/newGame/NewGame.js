3fa81ca1084980d8a09a63499f327f76618997ff
Emily Jones <efhjones@gmail.com>
2018-01-21 23:10:48 -0700

    swith AddPlayer to NewGame

--- pool-app/game/newGame/NewGame.js
+++ pool-app/game/newGame/NewGame.js
@@ -1,46 +1,202 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View
-} from 'react-native';
-import AddPlayerContainer from '../addPlayer/AddPlayerContainer';
+import PropTypes from 'prop-types';
+import { lifecycle, compose, withStateHandlers } from 'recompose';
+import _ from 'lodash';
+import { StyleSheet, View, Button, Text, TextInput, Modal } from 'react-native';
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    paddingTop: 100
   },
-  newGameHeaderContainer: {
-    display: 'flex',
-    height: 70,
-    backgroundColor: 'white',
-    width: '100%',
-    paddingTop: 30,
-    paddingRight: 20,
-    paddingBottom: 20,
-    paddingLeft: 20,
-    alignItems: 'center',
-    justifyContent: 'center'
+  field: {
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: 200,
+    marginBottom: 10,
+    padding: 10
   },
-  newGameHeader: {
-    color: 'darkgray',
-    fontSize: 20,
+  playersInGameContainer: {
+    width: '80%'
+  },
+  searchHeader: {
+
+  },
+  playersHeader: {
+    fontSize: 20
+  },
+  playerInGame: {
+    color: '#E8E9EB',
+    backgroundColor: '#202B4C',
+    alignSelf: 'flex-start',
+    padding: 5,
+    marginTop: 5,
+    marginBottom: 5,
     fontWeight: '600'
   },
-  addPlayerButton: {}
-
+  userBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 });
 
-const NewGame = () => (
+// enter search field for user
+// if no user found, add player button is shown
+// enter user email/name and click add player
+const NewGame = ({
+  onAddPlayer,
+  onSearchEmail,
+  onCloseSearchModal,
+  isSearchModalOpen,
+  email,
+  gameId,
+  filteredUsers,
+  onRemovePlayer,
+  onAddPlayerToTeam,
+  teams,
+  addingToTeamId,
+  onRemoveTeam,
+  onAddTeam
+}) => (
   <View style={styles.container}>
-    <AddPlayerContainer />
+    {
+      teams.map((team, i) => (
+        <View key={team.id}>
+          <Text style={styles.searchHeader}>Team {i + 1}</Text>
+          {
+            team.players.map(player => (
+              <View key={player.email} style={styles.userBar}>
+                <Button
+                  onPress={() => onRemovePlayer({ teamId: team.id, userId: player.id })}
+                  title="X"
+                  color="red"
+                />
+                <Text style={styles.playerInGame}>{player.email}</Text>
+              </View>
+            ))
+          }
+          <Button
+            onPress={() => onAddPlayerToTeam(team.id)}
+            title="+ Add Player"
+          />
+          { i > 1 && <Button
+            onPress={() => onRemoveTeam({ teamId: team.id, gameId })}
+            title="Remove Team"
+          />}
+        </View>
+      ))
+    }
+    <Button
+      onPress={() => onAddTeam(gameId)}
+      title="Add Team"
+    />
+    <Modal
+      visible={isSearchModalOpen}
+      animationType="slide"
+      onRequestClose={() => onCloseSearchModal()}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.innerContainer}>
+          <TextInput
+            onChangeText={onSearchEmail}
+            value={email}
+            style={styles.field}
+            autoCapitalize="none"
+            placeholder="Player Email"
+          />
+          {
+              filteredUsers.map(user => (
+                <Button
+                  key={user.email}
+                  onPress={() => {
+                    onAddPlayer({ gameId, userId: user.id, teamId: addingToTeamId });
+                    onCloseSearchModal();
+                  }}
+                  title={user.email}
+                />
+              ))
+            }
+        </View>
+      </View>
+    </Modal>
   </View>
 );
 
+
 NewGame.propTypes = {
+  onAddPlayer: PropTypes.func.isRequired,
+  onRemovePlayer: PropTypes.func.isRequired,
+  onRemoveTeam: PropTypes.func.isRequired,
+  onAddTeam: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
+  filteredUsers: PropTypes.arrayOf(PropTypes.object).isRequired,
+  isSearchModalOpen: PropTypes.bool.isRequired,
+  addingToTeamId: PropTypes.string,
+  onAddPlayerToTeam: PropTypes.func.isRequired,
+  onCloseSearchModal: PropTypes.func.isRequired,
+  onSearchEmail: PropTypes.func.isRequired,
+  gameId: PropTypes.string,
+  teams: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    players: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired
+    })).isRequired
+  })).isRequired
 };
 
+NewGame.defaultProps = {
+  gameId: null,
 
-export default NewGame;
+  addingToTeamId: null
+};
+
+const pathToGame = ['getGame', 'Game'];
+
+const initialState = {
+  email: '',
+  filteredUsers: [],
+  isSearchModalOpen: false,
+  addingToTeamId: null
+};
+
+const handlers = {
+  onAddPlayerToTeam: ({ isSearchModalOpen }) => teamId => (
+    { isSearchModalOpen: !isSearchModalOpen, addingToTeamId: teamId }
+  ),
+  onCloseSearchModal: () => () => ({
+    isSearchModalOpen: false,
+    addingToTeamId: null
+  }),
+  onSearchEmail: (state, { getAllUsers }) => (searchEmail) => {
+    const { allUsers } = getAllUsers;
+    const lowerCaseEmail = searchEmail.toLowerCase();
+    const filteredUsers = (allUsers || [])
+      .filter(({ email }) => email.toLowerCase().includes(lowerCaseEmail));
+    return {
+      filteredUsers,
+      email: searchEmail
+    };
+  }
+};
+
+export default compose(
+  withStateHandlers(initialState, handlers),
+  lifecycle({
+    componentWillMount() {
+      const isGameLoading = _.get(this.props, ['getGame', 'loading'], false);
+      const hasGameId = _.get(this.props, [...pathToGame, 'id'], false);
+      const game = _.get(this.props, pathToGame, null);
+      if (hasGameId && !isGameLoading) {
+        this.props.updateGame(game);
+      }
+    }
+  })
+)(NewGame);
+
--- pool-app/game/newGame/NewGameContainer.js
+++ pool-app/game/newGame/NewGameContainer.js
@@ -1,24 +1,83 @@
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { branch, renderComponent } from 'recompose';
 import NewGame from './NewGame';
-import createGame from '../../graphql/createGame';
+import GameActions from '../actions';
+import AppActions from '../../app/actions';
+import Loading from '../../loading/Loading';
+import withGame from '../../graphql/withGame';
+import withAllUsers from '../../graphql/withAllUsers';
+import addPlayerToTeam from '../../graphql/addPlayerToTeam';
+import removePlayerFromTeam from '../../graphql/removePlayerFromTeam';
+import removeTeam from '../../graphql/removeTeam';
+import addTeamToGame from '../../graphql/addTeamToGame';
 
 const mapStateToProps = (state) => {
-  const { app, game } = state;
-  const { gameId } = game;
-  const {
-    id
-  } = app;
-  const isNewGame = !gameId;
+  const { game } = state;
+  const { id, teams } = game;
   return {
-    navigation: state.nav,
-    id,
-    gameId,
-    isNewGame
+    gameId: id,
+    teams
   };
 };
 
+const mapDispatchToProps = (dispatch, props) => ({
+  onAddPlayer({ userId, teamId }) {
+    props.addPlayerToTeam({ variables: { userId, teamId } })
+      .then(({ data }) => {
+        const { addToPlayerInTeam } = data;
+        const team = _.get(addToPlayerInTeam, ['teamsTeam'], []);
+        dispatch(GameActions.updateTeamPlayers(team));
+      })
+      .catch((err) => {
+        dispatch(AppActions.onSetErrors(err));
+      });
+  },
+  onRemovePlayer({ teamId, userId }) {
+    props.removePlayerFromTeam({ variables: { teamId, userId } })
+      .then(({ data }) => {
+        const team = _.get(data, ['removeFromPlayerInTeam', 'teamsTeam']);
+        dispatch(GameActions.updateTeamPlayers(team));
+      }).catch((err) => {
+        dispatch(AppActions.onSetErrors(err));
+      });
+  },
+  onRemoveTeam({ teamId }) {
+    props.removeTeam({ variables: { teamId } })
+      .then(({ data }) => {
+        const id = _.get(data, ['deleteTeam', 'id']);
+        dispatch(GameActions.removeTeam(id));
+      }).catch((err) => {
+        dispatch(AppActions.onSetErrors(err));
+      });
+  },
+  onAddTeam(gameId) {
+    props.addTeamToGame({ gameId })
+      .then(({ data }) => {
+        const game = _.get(data, ['createTeam', 'game']);
+        dispatch(GameActions.updateGame(game));
+      }).catch((err) => {
+        dispatch(AppActions.onSetErrors(err));
+      });
+  },
+  updateGame(game) {
+    dispatch(GameActions.updateGame(game));
+  }
+});
+
 export default _.flowRight(
-  createGame,
-  connect(mapStateToProps)
+  addPlayerToTeam,
+  withAllUsers,
+  removePlayerFromTeam,
+  removeTeam,
+  addTeamToGame,
+  connect(mapStateToProps, mapDispatchToProps),
+  withGame,
+  branch(
+    (props) => {
+      const isGameLoading = _.get(props, ['getGame', 'loading'], false);
+      return isGameLoading;
+    },
+    renderComponent(Loading)
+  )
 )(NewGame);
