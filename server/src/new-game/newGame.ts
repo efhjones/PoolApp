cbda77ed01c99d5ab981748f0799f0d4e5884c68
Emily Jones <efhjones@gmail.com>
2018-01-22 02:01:08 -0700

    handle new game creation

--- server/src/new-game/newGame.ts
+++ server/src/new-game/newGame.ts
@@ -11,6 +11,23 @@ interface EventData {
   userId: string
 }
 
+interface NewTeam {
+  id: string,
+  players: Array<{ id: string, email: string}>,
+  gameId: string
+}
+
+interface Game {
+  id: string
+  teams: {
+    id: string,
+    players: {
+      id: string,
+      email: string
+    }
+  }
+}
+
 export default async (event: FunctionEvent<EventData>) => {
 
   try {
@@ -18,9 +35,12 @@ export default async (event: FunctionEvent<EventData>) => {
     const api = graphcool.api('simple/v1')
 
     const { userId } = event.data;
-
     // create new game
-    const gameId = await createNewGame(api, userId)
+    const gameId = await createNewGame(api, userId);
+
+    const inititalTeamOne = await createTeam(api, gameId);
+    const inititalTeamTwo = await createTeam(api, gameId);
+
     return { data: { id: gameId } }
   } catch (e) {
     return { error: 'Oh Nooooooooo! An unexpected error occured during new game creation.', e }
@@ -29,18 +49,55 @@ export default async (event: FunctionEvent<EventData>) => {
 
 async function createNewGame(api: GraphQLClient, userId: string): Promise<string> {
   const mutation = `
-    mutation {
-      createGame {
-        id
+    mutation createNewGame($userId: ID!) {
+      createGame(
+        creatorIdId: $userId,
+        inProgress: false
+      ) {
+        id,
+        teams {
+          id,
+          players {
+            id,
+            email
+          }
+        }
       }
     }
   `
 
   const variables = {
-    userId,
-  }
+    userId
+  };
 
   return api.request<{ createGame: NewGame }>(mutation, variables)
     .then((r => r.createGame.id))
 }
 
+async function createTeam(api: GraphQLClient, gameId: string): Promise<NewTeam> {
+  const mutation = `
+    mutation createTeam($gameId: ID!) {
+      createTeam(
+        gameId: $gameId
+      ) {
+        game {
+          teams {
+            id,
+            players {
+              id,
+              email
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const variables = {
+    gameId
+  };
+
+  return api.request<{ createTeam: NewTeam }>(mutation, variables)
+  .then((r => r.createTeam))
+
+}
